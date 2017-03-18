bc9c6edff560e0fe7364559ba8f428324d3f8a32
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2017-03-19 03:02:58 +0900

    [add] ACTを点灯

--- src/main.c
+++ src/main.c
@@ -1,13 +1,23 @@
 #include "rpi_lib/rpi.h"
 
-volatile int dataVal = 1;
-volatile int bssVal;
+#define GPFSEL0 0x3F200000
+#define GPFSEL1 0x3F200004
+#define GPFSEL2 0x3F200008
+#define GPFSEL4 0x3F200010
+#define GPSET0 0x3F20001C
+#define GPSET1 0x3F200020
+#define GPCLR0 0x3F200028
+#define GPCLR1 0x3F20002C
 
 void main(void)
 {
-  volatile const char const *rodataVal = "HogeHoge!";
-
   rpi_init();
+
+  *(volatile unsigned int *)GPFSEL4 = 0x01 << (3 * 7);
+  *(volatile unsigned int *)GPSET1 = 0x01 << 15;
+
+  while (1)
+    ;
 
   return;
 }
