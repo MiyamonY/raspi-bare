7b94781c4d4846d2888ba14a7da979f9a2550714
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-02-04 12:33:38 +0900

    [add] gpioのpull up/down設定関数を追加

--- src/lib/reg/reg.h
+++ src/lib/reg/reg.h
@@ -27,6 +27,31 @@ typedef struct {
   reg_t reserved2;
   reg_t GPCLR0;
   reg_t GPCLR1;
+  reg_t reserved3;
+  reg_t GPLEV0;
+  reg_t GPLEV1;
+  reg_t reserved4;
+  reg_t GPEDS0;
+  reg_t GPEDS1;
+  reg_t reserved5;
+  reg_t GPREN0;
+  reg_t GPREEN1;
+  reg_t reserved6;
+  reg_t GPFEN0;
+  reg_t GPFEN1;
+  reg_t reserved7;
+  reg_t GPHEN0;
+  reg_t GPHEN1;
+  reg_t reserved8;
+  reg_t GPAREN0;
+  reg_t GPAREN1;
+  reg_t reserved9;
+  reg_t GPAFEN0;
+  reg_t GPAFEN1;
+  reg_t reserved10;
+  reg_t GPPUD;
+  reg_t GPPUDCLK0;
+  reg_t GPPUDCLK1;
 } reg_gpio_t;
 
 typedef struct {
